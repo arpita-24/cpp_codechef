09f9be044990a4030d6fecca75cda5f0edc73e4f
Arpita Mohanty <89068505+arpita-24@users.noreply.github.com>
2024-04-21 21:53:59 +0530

    Update two_dishes.cpp
    Added the description of the coding problem

--- two_dishes.cpp
+++ two_dishes.cpp
@@ -1,3 +1,20 @@
+/*  
+The Two Dishes
+Chef prepared two dishes yesterday. Chef had assigned the tastiness 𝑇1 and T2 to the first and to the second dish respectively. The tastiness of the dishes can be any 
+integer between 0 and 𝑁 (both inclusive).
+Unfortunately, Chef has forgotten the values of  𝑇1 and T2 that he had assigned to the dishes. However, he remembers the sum of the tastiness of both the dishes - denoted by 
+𝑆.
+
+Chef wonders, what can be the maximum possible absolute difference between the tastiness of the two dishes. Can you help the Chef in finding the maximum absolute difference?
+
+
+Input Format
+The first line of input contains a single integer T, denoting the number of testcases. The description of the T testcases follows.
+The first and only line of each test case contains two space-separated integers 𝑁, S, denoting the maximum tastiness and the sum of tastiness of the two dishes, respectively.
+
+Output Format
+For each testcase, output a single line containing the maximum absolute difference between the tastiness of the two dishes.
+*/
 #include <bits/stdc++.h>
 using namespace std;
 
